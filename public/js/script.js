d68287d13703d2f6f4898dc750d7e9e119d1ec76
ViplavKhode <yesviplavkhode@gmail.com>
2025-12-28 21:38:44 -0500

    feat: implement marker for real-time location display on map

--- public/js/script.js
+++ public/js/script.js
@@ -17,8 +17,22 @@ if (navigator.geolocation) {
         });
 }
 
-const map = L.map("map").setView([0, 0], 10);
+const map = L.map("map").setView([0, 0], 16);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: 'Spot-On by Viplav Khode'
 }).addTo(map);
+
+const markers = {};
+
+socket.on("receive-location", (data) => {
+    const { id, latitude, longitude } = data;
+    map.setView([latitude, longitude], 16);
+
+    if (markers[id]) {
+        markers[id].setLatLng([latitude, longitude]);
+    } else {
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+});
+
